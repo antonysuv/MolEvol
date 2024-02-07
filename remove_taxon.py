f0012f446ef8a2b198b5b0aa7b42eea826477751
Anton Suvorov <antony.suvorov@gmail.com>
2024-02-07 14:51:57 -0500

    Update remove_taxon.py

--- remove_taxon.py
+++ remove_taxon.py
@@ -1,7 +1,23 @@
 from Bio import SeqIO
 
-sequences = SeqIO.parse('test.fasta', 'fasta')
-filtered = [seq for seq in sequences if 'VRE32514' not in seq.id]
 
-with open('output.fasta', 'wt') as output:
+def main():
+    parser = argparse.ArgumentParser(description='remove sequence from MSA fasta')
+    parser.add_argument( '--in', help = "input file in fasta",dest='FASTA')
+    parser.add_argument( '--ta', help = "taxon name to remove",dest='TAXON')
+  
+    
+    args = parser.parse_args()
+    
+    print("Reading input")
+    test_data = np.load(args.TEST)
+
+
+sequences = SeqIO.parse(args.FASTA, 'fasta')
+filtered = [seq for seq in sequences if args.TAXON not in seq.id]
+
+with open(args.FASTA, 'wt') as output:
     SeqIO.write(filtered, output, 'fasta')
+
+if __name__ == "__main__":
+    main()
